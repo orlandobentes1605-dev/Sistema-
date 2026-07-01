61d0f44e017bec9b0e93e172ed6000adc325c5bd
orlandobentes1605-dev <orlandobentes1605@gmail.com>
2026-07-01 16:30:06 -0400

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAC3xFRezQ-st2JCmUyCG7xMZAUZ5gIjyA",
+  authDomain: "sistemafiliacao-team-as.firebaseapp.com",
+  projectId: "sistemafiliacao-team-as",
+  storageBucket: "sistemafiliacao-team-as.firebasestorage.app",
+  messagingSenderId: "727325899527",
+  appId: "1:727325899527:web:3b4105e30e1f901775e3a2",
+  measurementId: "G-RWTHQZPR3W"
+};
